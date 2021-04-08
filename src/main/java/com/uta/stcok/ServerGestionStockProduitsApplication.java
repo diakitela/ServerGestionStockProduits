d663a2fdca4edf05c6bd4e4e679c2979b2b1ece2
diakitela <lassacom@gmail.com>
2021-04-08 09:16:02 +0100

    first commit

---
+++ src/main/java/com/uta/stcok/ServerGestionStockProduitsApplication.java
@@ -0,0 +1,13 @@
+package com.uta.stcok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerGestionStockProduitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerGestionStockProduitsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uta/stcok/ServerGestionStockProduitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uta.stcok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerGestionStockProduitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
